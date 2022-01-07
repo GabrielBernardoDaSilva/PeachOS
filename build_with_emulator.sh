0a3d88ea6fd50b502d15a67dd0506d109719aa77
Gabriel Bernardo <gabriel7798@gmail.com>
2022-01-07 19:22:41 -0300

    kernal printing

---
+++ build_with_emulator.sh
@@ -0,0 +1,11 @@
+#bin/bash
+
+make clean
+
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+make all
+
+qemu-system-x86_64 -hda ./bin/os.bin
--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -61,19 +61,60 @@ gdt_descriptor:
 
 [BITS 32]
 load32:
-    mov ax, DATA_SEG
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov ebp, 0x00200000
-    mov esp, ebp
-                                    ;enable a20 line
-    in al, 0x92
-    or al, 2
-    out 0x92, al
-    jmp $
+    mov eax, 0x01
+    mov ecx, 0x64
+    mov edi, 0x0100000
+    call ata_lbl_read
+    jmp CODE_SEG: 0x0100000
+
+ata_lbl_read:
+    mov ebx, eax                    ;backup the lba
+    shr eax, 24                     ;send the highest 8 bits of lba to hard disk controller shr => bitwise right 24 bits of 32 result only left 8 bits
+    or eax, 0xe0                    ;select master driver
+    mov dx, 0x1f6
+    out dx, al                      ;finished sending highest 8 bits of the lba
+
+
+    mov eax, ecx                    ;send total sectors to read
+    mov dx, 0x1f2
+    out dx, al                      ;finish sending total sectors to read
+
+    mov eax, ebx                    ;restore the backup lba
+    mov dx, 0x1f3
+    out dx, al                      ;finished sending more bits to lba
+
+    mov dx, 0x1f4                   ;sending more bits of the lba
+    mov eax, ebx                    ;restore the backup lba
+    shr eax, 8
+    out dx, al                      ;finished sending more bits to lba
+
+    mov dx, 0x1f5                   ;send upper 16bits to lba
+    mov eax, ebx
+    shr eax, 16
+    out dx, al                      ;finished sending upper 16bits to lba
+
+    mov dx, 0x1f7
+    mov al, 0x20
+    out dx, al
+
+.next_sector:                       ;read all sctors
+    push ecx
+
+.try_again:                         ;checkin if we need to read again
+    mov dx, 0x1f7
+    in al, dx
+    test al, 8
+    jz .try_again   
+                                    ;we need to read 256 words
+    mov ecx, 256
+    mov dx, 0x1f0
+    rep insw
+    pop ecx
+    loop .next_sector
+                                    ;ending reading sectors
+    ret 
+
+
 
 times 510-($ - $$) db 0
 dw 0xAA55 
