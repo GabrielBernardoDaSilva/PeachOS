93c46707d128d4d2c55c3e2b3b55de98abea7eb7
Gabriel Bernardo <gabriel7798@gmail.com>
2022-01-27 17:32:38 -0300

    fread function

--- src/fs/file.h
+++ src/fs/file.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "pparser.h"
+#include "stdint.h"
 
 typedef unsigned int FILE_SEEK_MODE;
 enum
@@ -19,6 +20,8 @@ enum
 };
 struct disk;
 typedef void*(*FS_OPEN_FUNCTION)(struct disk* disk, struct path_part* path, FILE_MODE mode);
+typedef int(*FS_READ_FUNCTION)(struct disk* disk, void* private, uint32_t size, uint32_t nmemb, char* out);
+
 typedef int(*FS_RESOLVE_FUNCTION)(struct disk* disk);
 
 
@@ -27,7 +30,7 @@ struct filesystem
     //filesystem should return zero from resolve if the provided disk is using its filesystem
     FS_RESOLVE_FUNCTION resolve;
     FS_OPEN_FUNCTION open;
-
+    FS_READ_FUNCTION read;
     char name[20];
 };
 
@@ -48,5 +51,6 @@ struct file_descriptor
 
 void fs_init();
 int fopen(const char* filename, const char* mode_str);
+int fread(void* ptr, uint32_t size, uint32_t nmemb, int fd);
 void fs_insert_filesystem(struct filesystem* filesystem);
 struct filesystem* fs_resolve(struct disk* disk);
