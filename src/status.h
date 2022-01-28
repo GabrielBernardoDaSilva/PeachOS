f4dce7fbe9ec957eca502805ff0780d92b46fd52
Gabriel Bernardo <gabriel7798@gmail.com>
2022-01-28 16:01:51 -0300

    fseek

--- src/status.h
+++ src/status.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#define PEACHOS_ALL_OK 0
-#define EIO 1
-#define EINVARG 2
-#define ENOMEM 3
-#define EBADPATH 4
-#define EFSNOTUS 5
-#define ERDONLY 6
+#define PEACHOS_ALL_OK          0
+#define EIO                     1
+#define EINVARG                 2
+#define ENOMEM                  3    
+#define EBADPATH                4
+#define EFSNOTUS                5
+#define ERDONLY                 6
+#define EUNIMP                  7
