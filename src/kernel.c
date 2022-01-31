f22a83e7c7955b90db48ac1b6d4898edb10a0a34
Gabriel Bernardo <gabriel7798@gmail.com>
2022-01-31 16:41:00 -0300

    start task implementation

--- src/kernel.c
+++ src/kernel.c
@@ -12,6 +12,7 @@
 #include "disk/streamer.h"
 #include "fs/file.h"
 #include "gdt/gdt.h"
+#include "task/tss.h"
 #include "config.h"
 
 uint16_t* video_mem = 0;
@@ -72,15 +73,21 @@ static struct paging_4gb_chunk* kernel_chunk = 0;
 void panic(const char* msg)
 {
     print(msg);
-    while(1){}
-}
+    while (1){}
+      
+}        
 
+
+struct tss tss;
 struct gdt gdt_real[PEACHOS_TOTAL_GDT_SEGMENTS];
 struct gdt_structured gdt_structured[PEACHOS_TOTAL_GDT_SEGMENTS] = 
 {
-    {.base = 0x00, .limit = 0x00, .type = 0x00},                        // NULL Segment
-    {.base = 0x00, .limit = 0xffffffff, .type = 0x9a},                  // Kenel code segment
-    {.base = 0x00, .limit = 0xffffffff, .type = 0x092},                 // Kernel data segment  
+    {.base = 0x00,           .limit = 0x00,       .type = 0x00},                        // NULL segment
+    {.base = 0x00,           .limit = 0xffffffff, .type = 0x9a},                  // Kenel code segment
+    {.base = 0x00,           .limit = 0xffffffff, .type = 0x92},                  // Kernel data segment  
+    {.base = 0x00,           .limit = 0xffffffff, .type = 0xf8},                  // User code segment
+    {.base = 0x00,           .limit = 0xffffffff, .type = 0xf2},                  // User data segment
+    {.base = (uint32_t)&tss, .limit = sizeof(tss),.type = 0xe9}, 
 };
  
 void kernel_main()
@@ -105,6 +112,14 @@ void kernel_main()
     
     // Initialize the interrupt descriptor table
     idt_init();
+
+    // Setup the tss
+    memset(&tss, 0x00, sizeof(tss));
+    tss.esp0 = 0x600000;
+    tss.ss0 = KERNAL_DATA_SELECTOR;
+
+    // Load tss
+    tss_load(0x28);
 
     // Setup paging
     kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
